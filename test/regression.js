750afead001ce9ba654b82b301f0260df2bbe005
Cory Mawhorter <cory.mawhorter@gmail.com>
2017-06-12 13:58:58 -0700

    fix for node 0.12

--- test/regression.js
+++ test/regression.js
@@ -5,7 +5,7 @@ var waterfall = require('../dist/waterfall.umd.js');
 describe('regression tests', function() {
   it('https://github.com/cmawhorter/waterfall/issues/1', function() {
     assert.throws(function() {
-      waterfall({}, () => {
+      waterfall({}, function() {
         throw new Error('test123');
       });
     }, 'test123');
--- test/waterfall.js
+++ test/waterfall.js
@@ -4,7 +4,7 @@ var waterfall = require('../dist/waterfall.umd.js');
 
 describe('waterfall', function() {
   it('should take steps and return a result using an object literal', function(done) {
-    waterfall({}, (err, state) => {
+    waterfall({}, function(err, state) {
       assert.ifError(err);
       expect(state).toBeAn(Object);
       expect(Object.keys(state).length).toEqual(0);
@@ -12,7 +12,7 @@ describe('waterfall', function() {
     });
   });
   it('should take steps and return a result using an array', function(done) {
-    waterfall([], (err, state) => {
+    waterfall([], function(err, state) {
       assert.ifError(err);
       expect(state).toBeAn(Object);
       expect(Object.keys(state).length).toEqual(0);
@@ -21,8 +21,8 @@ describe('waterfall', function() {
   });
   it('should assign result to associated key for hash', function(done) {
     waterfall({
-      test: (state, next) => next(null, 'hello'),
-    }, (err, state) => {
+      test: function(state, next) { next(null, 'hello') },
+    }, function(err, state) {
       assert.ifError(err);
       expect(state.test).toEqual('hello');
       done();
@@ -30,8 +30,8 @@ describe('waterfall', function() {
   });
   it('should assign result to associated key for array', function(done) {
     waterfall([
-      (state, next) => next(null, 'hello'),
-    ], (err, state) => {
+      function(state, next) { next(null, 'hello') },
+    ], function(err, state) {
       assert.ifError(err);
       expect(state['0']).toEqual('hello');
       done();
@@ -39,15 +39,15 @@ describe('waterfall', function() {
   });
   it('should take an optional starting state', function(done) {
     waterfall({
-      test: (state, next) => expect(state.hello).toEqual('world') && next(null),
+      test: function(state, next) { expect(state.hello).toEqual('world') && next(null) },
     }, { hello: 'world' }, done);
   });
   it('should appropriately catch thrown errors', function(done) {
     waterfall({
-      test: (state, next) => {
+      test: function(state, next) {
         throw new Error('good');
       },
-    }, { hello: 'world' }, (err) => {
+    }, { hello: 'world' }, function(err) {
       expect(err).toBeAn(Error);
       expect(err.message).toEqual('good');
       done();
@@ -55,8 +55,10 @@ describe('waterfall', function() {
   });
   it('should appropriately handle error responses', function(done) {
     waterfall({
-      test: (state, next) => next(new Error('good')),
-    }, { hello: 'world' }, (err) => {
+      test: function(state, next) {
+        next(new Error('good'));
+      },
+    }, { hello: 'world' }, function(err) {
       expect(err).toBeAn(Error);
       expect(err.message).toEqual('good');
       done();
